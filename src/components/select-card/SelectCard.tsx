175d4279c5e29b355ec18aef54a55618f21462fa
Alexander <forcewizu@gmail.com>
2019-09-21 04:20:24 +0300

    new UX for nobile

--- src/components/select-card/SelectCard.tsx
+++ src/components/select-card/SelectCard.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Select } from 'antd';
 import { setExchange } from '../../store/actions';
 import { connect } from 'react-redux';
-import { Currencies, Exchange } from '../../types';
+import { Currencies, Exchange, SN } from '../../types';
 import { ExchangesState } from '../../store/types';
+import getIcon from '../../utils/getIcon';
+import appLogo from '../../assets/images/currencies.svg';
+
 const { Option }: any = Select;
 
 const filterOption = (inputValue: string, option: any) => {
@@ -23,9 +26,37 @@ interface SelectCardProps {
 }
 
 const SelectCard = ({ exchange, currencies, loading, setExchange }: SelectCardProps) => {
-  
+
+  const {
+    currencyA: { code: codeA, currency: currencyA, country: countryA = '' },
+    currencyB: { code: codeB, currency: currencyB, country: countryB = '' }
+  } = exchange as Exchange;
+
+  const [sPosition, setSelectPosition] = useState(50);
+
+  const handleScroll = (e: any) => {
+    
+    const testDiv = document.getElementById("select");
+    if (testDiv) {
+      const rect: any = testDiv.getBoundingClientRect();
+      setSelectPosition(rect.top);
+    }
+  }
+
+  useEffect(
+    () => {
+      window.addEventListener('scroll', handleScroll);
+      return () => window.removeEventListener('scroll', handleScroll);
+    },
+    []
+  )
+
+  useEffect(
+    () => console.log(sPosition)
+  )
+
   return (
-    <div className="select-card">
+    <div className="select-card" onScroll={handleScroll}>
       <p className="select-card__title">Сurrency selection</p>
       <Select
         showSearch
@@ -45,21 +76,49 @@ const SelectCard = ({ exchange, currencies, loading, setExchange }: SelectCardPr
           return (<Option key={id} value={id} label={code}><b>{currencyB.code}</b> - <b>{code}</b> {currency}</Option>)
         })}
       </Select>
-      <select
-        name='currencies'
-        value={exchange.id}
-        onChange={(e: any) => {
-          const v = e.target.value;
-          setExchange(v);
-        }}
-        className='select-card__select-input'>
-        {
-          currencies.map((item) => { 
-            const { currencyA: { code, currency }, id, currencyB, }: Exchange = item;
-            return (<option key={id} value={id}>{currencyB.code} - {code} {currency}</option>)
-          })
-        }
-      </select>
+      <div className={`select-card__select-wrapper ${sPosition < 30 ? 'active': ''}`} id='select'>
+        <div className='select-card__select-inner'>
+          <span className='select-card__help'>Tap here to select currencies</span>
+          <div className='select-card__placeholder'>
+            <p className='select-card__caption'>UAH Excahnger</p>
+            <div className='select-card__placeholder-item'>
+              <object
+                type="image/svg+xml"
+                data={getIcon(countryA, codeA)}
+                className="select-card__icon select-card__icon--m-right"
+              >
+              </object>
+              { currencyA }
+            </div>
+            <i className="fas fa-exchange-alt select-card__icon-middle"></i>
+            <div className='select-card__placeholder-item select-card__placeholder-item--right'>
+            { currencyB }
+              <object
+                type="image/svg+xml"
+                data={getIcon(countryB, codeB)}
+                className="select-card__icon select-card__icon--m-left"
+              >
+              </object>
+              
+            </div>
+          </div>
+          <select
+            name='currencies'
+            value={exchange.id}
+            onChange={(e: any) => {
+              const v = e.target.value;
+              setExchange(v);
+            }}
+            className='select-card__select-input'>
+            {
+              currencies.map((item) => { 
+                const { currencyA: { code, currency }, id, currencyB, }: Exchange = item;
+                return (<option key={id} value={id}>{currencyB.code} - {code} {currency}</option>)
+              })
+            }
+          </select>
+        </div>
+      </div>
     </div>
   );
 };
