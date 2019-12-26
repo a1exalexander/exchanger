2963c2a40b04e1c60419c4bb7cf64154bd840027
Alexander Ratushnyi <forcewizu@gmail.com>
2019-12-26 09:35:49 +0200

    :fire: Remove btn

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, FC } from "react";
-import classNames from "classnames";
 import { connect } from "react-redux";
 import { fetchCurrencies } from "../store/actions";
 import { HomePage } from "../pages";
@@ -9,7 +8,6 @@ import "antd/es/select/style/css";
 import "antd/es/dropdown/style/css";
 import "antd/es/skeleton/style/css";
 import { ExchangesState } from "../store/types";
-import moment from "moment";
 import { needUpdate } from "../utils/helpers";
 
 interface IStateProps {
@@ -17,7 +15,6 @@ interface IStateProps {
   computedCurrency: object;
   exchange: object;
   method: string;
-  loading: boolean;
 }
 interface IDispatchProps {
   fetchCurrencies: Function;
@@ -26,42 +23,27 @@ interface IDispatchProps {
 type IProps = IStateProps & IDispatchProps;
 
 const App: FC<IProps> = props => {
-  const { loading, method, computedCurrency, exchange, lastUpdate, fetchCurrencies } = props;
+  const {
+    method,
+    computedCurrency,
+    exchange,
+    lastUpdate,
+    fetchCurrencies
+  } = props;
 
   useEffect(() => {
     if (needUpdate(lastUpdate)) fetchCurrencies();
   });
 
   useEffect(() => {
-    console.log(lastUpdate)
+    console.log(lastUpdate);
     if (needUpdate(lastUpdate)) fetchCurrencies();
   }, [lastUpdate, method, computedCurrency, fetchCurrencies, exchange]);
 
   useEffect(() => {
     fetchCurrencies();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  const extra = () => {
-    if (
-      moment(lastUpdate, "DD.MM.YYYY hh:mm").isValid() &&
-      moment().diff(moment(lastUpdate, "DD.MM.YYYY hh:mm"), "hours") > 1
-    ) {
-      return (
-        <button className={classNames("update-button", { loading })}>
-          {loading ? "loading" : "Update exchange rates"}
-        </button>
-      );
-    } else if (lastUpdate) {
-      return (
-        <div className="app__date-card">
-          <span>Last update:</span>{" "}
-          <span className="app__date">{lastUpdate}</span>
-        </div>
-      );
-    }
-    return null;
-  };
 
   return (
     <div className="app">
@@ -70,12 +52,24 @@ const App: FC<IProps> = props => {
       <div className="app__carousel">
         <AppCarousel />
       </div>
-      <footer className="app__footer">{extra()}</footer>
+      <footer className="app__footer">
+        {lastUpdate && (
+          <div className="app__date-card">
+            <span>Last update:</span>{" "}
+            <span className="app__date">{lastUpdate}</span>
+          </div>
+        )}
+      </footer>
     </div>
   );
 };
 
 export default connect<IStateProps, IDispatchProps, object, ExchangesState>(
-  ({ computedCurrency, method, exchange, lastUpdate, loading }) => ({ method, computedCurrency, exchange, lastUpdate, loading }),
+  ({ computedCurrency, method, exchange, lastUpdate }) => ({
+    method,
+    computedCurrency,
+    exchange,
+    lastUpdate,
+  }),
   { fetchCurrencies }
 )(App);
