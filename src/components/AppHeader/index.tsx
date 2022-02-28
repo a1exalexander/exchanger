3cbb1b40b11f11625cd20ef9729e1995e525f360
Alexander Ratushnyi <alex@merge.rocks>
2022-02-28 11:22:06 +0200

    ✨ Idi nahui

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { AppCarousel } from '../carousel';
-import { ReactComponent as AppLogo } from '../../assets/images/currencies.svg';
+// import { ReactComponent as AppLogo } from '../../assets/images/currencies.svg';
 
 export const AppHeader: FC = () => {
   return (
@@ -8,8 +8,8 @@ export const AppHeader: FC = () => {
       <span className="app-header__help">Натисни тут, щоб вибрати валюту</span>
       <nav className="app-header__nav">
         <a href="/" className="app-header__link">
-          <AppLogo className="app-header__logo" />
-          <h2 className="app-header__title">UAH Exchanger</h2>
+          {/* <AppLogo className="app-header__logo" /> */}
+          <h2 className="app-header__title">Русский корабль, иди нахуй!</h2>
         </a>
       </nav>
       <AppCarousel />
